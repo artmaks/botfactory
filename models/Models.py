60e23d56fdab0fa01cde33c27ec73503f92b5a06
Artem Maksimov <temamaksimov@gmail.com>
2017-03-13 22:08:33 +0300

    Add new parameter to bot in our system (api namespace)

--- models/Models.py
+++ models/Models.py
@@ -6,4 +6,5 @@ class BotModel(ndb.Model):
     name = ndb.StringProperty(default='')
     token = ndb.StringProperty(default='')
     link = ndb.StringProperty(default='')
+    api_namespace = ndb.StringProperty(default='')
 
