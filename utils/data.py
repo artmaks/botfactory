7d2af73f0dbeb164fda5b379caf09da827cac05f
cfga <bibik.platon@yandex.ru>
2017-03-15 01:02:56 +0300

    NavigationState

--- utils/data.py
+++ utils/data.py
@@ -2,6 +2,8 @@
 from models.Models import *
 
 # Получить данные по имени бота (имя - ссылка в телеграмме)
+
+
 def getBotDataByName(name):
-    bots = [p.to_dict() for p in BotModel.query(BotModel.link == name).fetch()]
+    bots = [p.to_dict() for p in Bot.query(Bot.link == name).fetch()]
     return bots[0]
