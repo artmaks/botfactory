d790593a320486a02df510871a5c11b5f926e74b
cfga <bibik.platon@yandex.ru>
2017-03-14 19:09:47 +0300

    Add order creation and filling

--- models/Models.py
+++ models/Models.py
@@ -2,13 +2,35 @@
 from google.appengine.ext import ndb
 
 
-class BotModel(ndb.Model):
+class Bot(ndb.Model):
     name = ndb.StringProperty(default='')
     token = ndb.StringProperty(default='')
     link = ndb.StringProperty(default='')
     api_namespace = ndb.StringProperty(default='')
 
-class Users(ndb.Model):
+
+class User(ndb.Model):
     name = ndb.StringProperty(default='')
     chat_id = ndb.StringProperty(default='')
     api_user_id = ndb.StringProperty(default='')
+
+
+class Order(ndb.Model):
+    name = ndb.StringProperty(default='')
+    active = ndb.BooleanProperty(default=True)
+
+
+class OrderItem(ndb.Model):
+    name = ndb.StringProperty(default='')
+    count = ndb.IntegerProperty()
+
+
+class Category(ndb.Model):
+    name = ndb.StringProperty(default='')
+    description = ndb.StringProperty(default='')
+
+
+class Product(ndb.Model):
+    name = ndb.StringProperty(default='')
+    description = ndb.StringProperty(default='')
+    available = ndb.BooleanProperty(default=True)
