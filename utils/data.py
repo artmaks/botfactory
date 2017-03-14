f3632ac02100436c54b3fbcb1d5bb547d9403e28
Artem Maksimov <artem@getcodified.com>
2017-03-14 17:23:48 +0300

    Add utils folder, Add function for getting bot data by name, Add utils for check register and register new user

---
+++ utils/data.py
@@ -0,0 +1,6 @@
+from models.Models import *
+
+# Получить данные по имени бота (имя - ссылка в телеграмме)
+def getBotDataByName(name):
+    bots = [p.to_dict() for p in BotModel.query(BotModel.link == name).fetch()]
+    return bots[0]
