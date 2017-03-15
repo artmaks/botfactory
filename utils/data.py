0c9bda6fb65eae2e7f49f73b4426f28b090c4d34
Artem Maksimov <temamaksimov@gmail.com>
2017-03-15 21:51:45 +0300

    Add order state Modeal and GET/SET functions

--- utils/data.py
+++ utils/data.py
@@ -22,7 +22,7 @@ def resetMenuState(chat_id, message_id):
     state_entity = NavigationState(chat_id=str(chat_id), state='{"steps": []}', message_id = message_id)
     state_entity.put()
 
-
+#======= Menu ======
 def getCurrentMenuMessage(chat_id):
     state = [p.to_dict() for p in NavigationState.query(NavigationState.chat_id == str(chat_id)).fetch()]
     return state[0]['message_id'] if len(state) > 0 else None
@@ -45,3 +45,22 @@ def updateMenuStateByChatId(chat_id, new_state):
         state_entity.state = json_state
         state_entity.put()
 
+#======= Orders state ======
+
+def getOrderStateByChatId(chat_id):
+    state = [p.to_dict() for p in OrderState.query(OrderState.chat_id == str(chat_id)).fetch()]
+    return state[0]['state'] if len(state) > 0 else '{}'
+
+def updateOrderStateByChatId(chat_id, new_state):
+    json_state = json.dumps(new_state)
+    state = OrderState.query(OrderState.chat_id == str(chat_id)).fetch()
+    if len(state) != 1:
+        for l in state:
+            l.key.delete()
+
+        state_entity = OrderState(chat_id = str(chat_id), state = json_state)
+        state_entity.put()
+    else:
+        state_entity = state[0].key.get()
+        state_entity.state = json_state
+        state_entity.put()
